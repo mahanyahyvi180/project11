d61efd14a06b45caba2e3d7de38f2189f94e9b08
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-07-29 17:11:17 +0330

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (var counter = 0; counter < array.Length; ++counter)
+            {
+                array[counter] = initialValue + increment * counter;
+                Console.ReadLine();
+            }
+            Console.WriteLine($"{"Index"}{"Value,8"}");
+            Console.ReadLine();
+        
+                for (int counter = 0; counter < array.Length; ++counter)
+                {
+                    Console.WriteLine($"{counter,5}{array[counter],8}");
+                    Console.ReadLine();
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+
+    }
+
+
+   
